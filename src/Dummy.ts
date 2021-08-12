cc98f9c2cd428ac64ac98f8910e35b7414f6e9dd
Aman Gupta <aman.gupta.rkg6@gmail.com>
2021-08-12 10:41:27 +0530

    dummy tests

--- src/Dummy.ts
+++ src/Dummy.ts
@@ -1,3 +1,39 @@
 export const dummyOutput = (input: string) => {
     return input + input
 }
+
+export const primeCollector = (input: number) => {
+    
+    if(input == 1) return [];
+    
+    let n = input;
+    let l = [];
+    for(let i=2;i<=n;i++){
+        while(input % i === 0){
+            l.push(i);
+            input = input / i;
+        }
+    }
+
+    return l;
+}
+
+export const stoneGame = (n: number, a: number[]) => {
+    let f=0,l=0,x;
+    for(let i=0;i< a.length; i++){
+        if(i == 1) f = i + 1;
+        if(i == n) l = i + 1;
+    }
+
+    if(f > l){
+        let e = f;
+        f = l;
+        l = e;
+    }
+
+    return Math.min(
+        f + n - l + 1,
+        l,
+        n - f + 1
+    );
+}
--- test/src/Dummy.test.ts
+++ test/src/Dummy.test.ts
@@ -1,8 +1,32 @@
-import { dummyOutput } from '../../src/Dummy'
+import { dummyOutput, primeCollector, stoneGame } from '../../src/Dummy'
 
 describe("Dummy Test", () => {
     it("should assert on the dummy output", () => {
         const dummyOutputReturned = dummyOutput("hello")
         expect(dummyOutputReturned).toBe("hellohello")
+    })
+})
+
+describe("Prime Number Test", () => {
+    it("should print prime factors of the given number", () => {
+        const dummyOutputReturned = primeCollector(24);
+        expect(dummyOutputReturned).toEqual([2,2,2,3]);
+    })
+    it("should print prime factors of the given number", () => {
+        const dummyOutputReturned = primeCollector(36);
+        expect(dummyOutputReturned).toEqual([2,2,3,3]);
+        const dummyOutputReturned2 = primeCollector(1);
+        expect(dummyOutputReturned2).toEqual([]);
+    })
+    it("should print prime factors of the given number", () => {
+        const dummyOutputReturned = primeCollector(11);
+        expect(dummyOutputReturned).toEqual([11]);
+    })
+})
+
+describe("StoneGame test", () => {
+    it("should output StoneGame answer", () => {
+        const dummyOutputReturned = stoneGame(5, [1,5,4,3,2]);
+        expect(dummyOutputReturned).toEqual(2);
     })
 })
